254b72c52e9fee2c9c308ffcba5f4ec26cd206ec
Kaleb Elwert <belak@coded.io>
2023-03-15 12:46:13 -0700

    Upgrade dependencies

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,7 +2,7 @@ pub use std::sync::Arc;
 
 pub use anyhow::{format_err, Context as AnyhowContext};
 pub use log::{debug, error, info, warn};
-pub use tokio::stream::{Stream, StreamExt};
+pub use tokio_stream::{Stream, StreamExt};
 
 pub use seabird::proto;
 pub use seabird::proto::Event as SeabirdEvent;
