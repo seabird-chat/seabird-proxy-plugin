638a605e8efc046e6bbc8b41edefe04666613edd
Kaleb Elwert <belak@coded.io>
2024-01-23 08:21:45 -0800

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ (import rust-overlay) ];
+          };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+            ];
+
+            nativeBuildInputs = [
+              (pkgs.rust-bin.stable."1.71.1".default.override {
+                extensions = ["rust-src"];
+              })
+              pkgs.rust-analyzer
+            ];
+
+            RUST_BACKTRACE = 1;
+          };
+        }
+      );
+}
